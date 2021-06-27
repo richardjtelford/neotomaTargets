a4236d2ce060e8415b9574294588b7560034d29f
richardjtelford <richard.telford@bio.uib.no>
2021-06-27 16:12:17 +0200

    minimal analysis

--- R/analysis_plan.R
+++ R/analysis_plan.R
@@ -1,5 +1,20 @@
 #
 analysis_plan <- list(
   # do something interesting
+  # calculate count sums
+  tar_target(
+    name = count_sums,
+    command = pollen_data %>%
+      mutate(countsum = map(data, ~summarise(.x, sum = sum(count)))) %>%
+      select(-data) %>% #remove data to reduce size of object
+      unnest(cols = countsum)
+    ),
 
+    #summarise count sums
+    tar_target(
+      name = count_summary,
+      command = count_sums %>%
+        ungroup() %>%
+        summarise(n = n(), mn = min(sum), mx = max(sum))
+    )
 )
