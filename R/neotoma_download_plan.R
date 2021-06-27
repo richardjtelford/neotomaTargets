b9bbc6360e1db280afb2c31d5c3e8a89e359fc2e
richardjtelford <richard.telford@bio.uib.no>
2021-06-27 16:12:05 +0200

    download code

--- R/neotoma_download_plan.R
+++ R/neotoma_download_plan.R
@@ -2,6 +2,16 @@
 
 neotoma_download_plan <- list(
   #select datasets to download
-  # download datasets with branching
+  #get dataset list
+  tar_target(
+    name = pollen_sites,
+    command = get_dataset(datasettype = "pollen", gpid = "Norway")
+    ),
 
+  #download_data - slow. Uses dynamic branching
+  tar_target(
+    name = raw_pollen_data,
+    command =  pollen_sites %>% get_download(),
+    pattern = map(pollen_sites)
+  )
 )
