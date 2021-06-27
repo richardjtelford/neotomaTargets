88357bd2d3b3ed968c92ce6cb35f58991cd43b82
richardjtelford <richard.telford@bio.uib.no>
2021-06-27 16:11:52 +0200

    very minimal cleaning

--- R/cleaning_plan.R
+++ R/cleaning_plan.R
@@ -1,8 +1,17 @@
-#
+# try to make as few targets as possible as each target is cached.
+# With many intermediate steps, it uses a lot of disk space.
 
 cleaning_plan <- list(
-  # check countsums
-  # taxonomic harmonisation
-  # ?chronology control
+  # convert to tibble
+  tar_target(
+    name = pollen_data,
+    command = raw_pollen_data %>%
+      as_tibble() %>%
+      #remove algal etc counts
+      mutate(data = map(data, ~filter(.x, variable.element %in% c("pollen", "spore", "pollen/spore"))))
+  )
+  # ?remove samples with low countsums
+  # ?taxonomic harmonisation - join to harmonisation table
+  # ?removes sites with insufficient chronology control
 
 )
